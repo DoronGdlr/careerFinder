cae06da17b33fafc07614eb02a2d8ae03f134301
DoronGdlr <grayknightdoron@gmail.com>
2025-05-26 01:42:38 +0300

    Create hardcodedData.js

---
+++ data/hardcodedData.js
@@ -0,0 +1,299 @@
+// Hardcoded job data with company layer
+export const hardcodedJobData = {
+  companies: [
+    {
+      name: 'CheckPoint',
+      description: `
+        <div class="job-description">
+          <h4>About CheckPoint</h4>
+          <p>At Check Point, we're committed to excellence in cybersecurity solutions. Our innovative technologies protect organizations and consumers from cyber attacks with an industry leading catch rate of malware and other types of threats.</p>
+          <p>Founded in 1993, Check Point is a leading provider of cyber security solutions to governments and corporate enterprises globally.</p>
+        </div>
+      `,
+      jobs: [
+        {
+          Department: 'Products - R&D',
+          positions: [
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=22854&referer=KldpYuV5SQNGRMTyOiq7yickdoBsvMUpvM3ayk56v31XP5x57YiyvqM7DDsieHAj',
+                requisition: '22854',
+                position: 'AI Engineer (22854)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23020&referer=DGNlM90DUeS45DdH1mJjh+jzgwuzh745YuJNQE/vAw6PSLxtmZa7eBckRpjQf/wA',
+                requisition: '23020',
+                position: 'AI Engineering Team Lead (23020)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23077&referer=26pVEGQAHknTEwrsXk6LrUKJthBnvkfQ3V8EnH6/9N6h3pS2RUAbH9gx3uIXO9+e',
+                requisition: '23077',
+                position: 'Data Engineer (23077)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=22431&referer=cQG1E9hwt66RKIeX/MbCnRUe66011Py/TuKSKD53DZDwU5AasS40xTIsOdecifq3',
+                requisition: '22431',
+                position: 'Data Scientist (22431)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23290&referer=DKJZ0twXm5C4YM5bEXTs1l8BfuSOLNKgkHHfgwHmA3cUO9QOjicuWzeGGR4Oimc2',
+                requisition: '23290',
+                position: 'Data Team Lead (23290)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23046',
+                position: 'DevOps Engineer Infinity Next Platform (23046)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '21856',
+                position: 'Frontend Developer (21856)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22788',
+                position: 'Full Stack Developer (22788)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23150',
+                position:
+                  'Full Stack Developer (Frontend oriented)- Cloud & Web Management (23150)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '21963',
+                position: 'Full Stack Software Developer (21963)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22511',
+                position: 'Full Stack Software Engineer (22511)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22995',
+                position: 'Malware Analysis Team Leader (22995)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22994',
+                position: 'Mobile Group Manager (22994)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23038',
+                position: 'OS Core Team Leader (23038)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23076',
+                position: 'Product Manager - CloudGuard WAF (23076)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23232',
+                position: 'Senior FinOps Engineer (23232)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22359',
+                position: 'Senior Full-Stack Developer (22359)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '20959',
+                position: 'Senior Software Developer - XDR Analytics (20959)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23191',
+                position: 'Software Developer (23191)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '21678',
+                position:
+                  'Software Development Team Leader - DSP Group (21678)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22794',
+                position: 'Technical Project Manager (22794)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22879',
+                position: 'Threat Intelligence Researcher (22879)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22883',
+                position: 'Windows C++ Developer (22883)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22884',
+                position: 'Windows Internals Software Developer (22884)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '21905',
+                position: 'XDR Full Stack Developer (21905)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23021&referer=3h2FHNxvZNfzZQ5Di2wPESWE1Huk0hqzq3xbBlSFnosegddM6s0edrQdJhUR99M9',
+                requisition: '23021',
+                position:
+                  'Domain Expert Network Management & Administration (23021)',
+              },
+            },
+          ],
+        },
+        {
+          Department: 'Security Service Edge',
+          positions: [
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23285&referer=3h2FHNxvZNfzZQ5Di2wPESWE1Huk0hqzq3xbBlSFnosegddM6s0edrQdJhUR99M9',
+                requisition: '23285',
+                position: 'C++/GO Developer (23285)',
+              },
+            },
+            {
+              position: {
+                url: 'https://careers.checkpoint.com/index.php?m=cpcareers&a=show&joborderid=23031&referer=nfDL8MICJ20bTIEXSNxkSNn/gfUJGz+w8kO9x6i6117tsf4fspmXZjwg8nyaO7It',
+                requisition: '23031',
+                position: 'Data and Software Engineering Team Leader (23031)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22300',
+                position: 'DevOps Team Leader (22300)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22710',
+                position: 'Infrastructure Engineer (22710)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22675',
+                position: 'macOS C++ Developer (22675)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '23345',
+                position: 'Networking Full Stack Technology Leader (23345)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22846',
+                position: 'Site Reliability Team Leader (22846)',
+              },
+            },
+          ],
+        },
+        {
+          Department: 'Harmony Email Security',
+          positions: [
+            {
+              position: {
+                url: '',
+                requisition: '23189',
+                position: 'Full-Stack Team Leader (23189)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '21075',
+                position: 'Senior Back End Developer (21075)',
+              },
+            },
+          ],
+        },
+        {
+          Department: 'Cloud Security',
+          positions: [
+            {
+              position: {
+                url: '',
+                requisition: '21889',
+                position: 'Software Developer - Cloud Security (21889)',
+              },
+            },
+            {
+              position: {
+                url: '',
+                requisition: '22680',
+                position: 'Software Project Manager - Cloud Security (22680)',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
